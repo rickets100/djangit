f96fc588677668be7823bbb6b443ba6d49916a04
Kay Krewson <kay.k@comcast.net>
2017-05-21 15:21:09 -0700

    trying in vain to get a post/add route to work

--- djangit/views.py
+++ djangit/views.py
@@ -5,6 +5,7 @@
 from djangit.forms import HopForm
 from djangit.models import Hop
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -21,6 +22,9 @@ def index(request):
 # ===== GET ALL =====
 def hops(request):
     print "got to get all"
+    print "request.method is"
+    print request.method
+
     all_hops = Hop.objects.all().order_by('hop_name')
     template = loader.get_template('hops.html')
     model =  {
@@ -34,23 +38,27 @@ def edithop(request, id):
     print 'got to edithop'
     hop = Hop.objects.get(id=id)
     hopform = HopForm(instance=hop)
-    template = loader.get_template('hopform.html')
+    # template = loader.get_template('hopform.html')
     model =  {
         "hopform":hopform
         }
+    # context_instance=RequestContext(request, model)
 
-    return HttpResponse(template.render(model))
+    return render(request, 'hopform.html', model)
 
 # ===== NEW HOP FORM =====
+# django bug?
+# https://code.djangoproject.com/ticket/27722?cversion=0&cnum_hist=3
 def newhop(request):
     print 'got to newhop'
     hopform = HopForm()
-    template = loader.get_template('hopform.html')
+    # template = loader.get_template('hopform.html')
     model =  {
-        "hopform":hopform
+        'hopform':hopform
         }
+    # context_instance=RequestContext(request, model)
 
-    return HttpResponse(template.render(model))
+    return render(request, 'hopform.html', model)
 
 # ===== GET ONE BY ID =====
 def onehop(request, id):
@@ -65,27 +73,30 @@ def onehop(request, id):
     template = loader.get_template('showhop.html')
     return HttpResponse(template.render(model))
 
-# ===== ADD A HOP =====
-def addhop(request, id):
-    print 'got to add a shop'
-    template = loader.get_template('showhop.html')
-    context_instance=RequestContext(request)
-    hop = Hop.objects.get(id=id)
-    model =  {
-        "hop":hop
-        }
-    model.save()
+# ===== ADD A NEW HOP =====
+def addhop(request):
+    print 'got to addhop'
+    print 'request body is'
+    print request.body
 
-    return HttpResponse(template.render(model))
+    template = loader.get_template('hops.html')
+    # hop = Hop.objects.get()
+    # model =  {
+    #     "hop":hop
+    #     }
+    # hop.save()
+
+    return HttpResponse(template.render())
 
 # ===== DELETE ONE =====
 def deletehop(request, id):
     print 'got to deletehop'
     template = loader.get_template('hops.html')
+    all_hops = Hop.objects.all().order_by('hop_name')
     hop = Hop.objects.get(id=id)
-    model =  {
-        "hop":hop
-        }
     hop.delete()
+    model =  {
+        "all_hops":all_hops
+        }
 
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(model))
